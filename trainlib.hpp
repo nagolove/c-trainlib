e19f01faa82584a2369e41bd7d6f8c8df722af63
nagolove <dekar.own@gmail.com>
2021-07-13 15:37:27 +0000

    new version.

--- trainlib.hpp
+++ trainlib.hpp
@@ -2,69 +2,71 @@
  * Библиотека для помощи в решении задач с HackerRank. Используется методом
  * копипаста в начало файла исходного кода. Черновик.
  */
-
 #include <string>
+#include <regex>
 #include <sstream>
 #include <cmath>
 #include <cstdio>
 #include <vector>
 #include <iostream>
 #include <algorithm>
+
 using namespace std;
 
 typedef vector<string> StringVector;
 
-StringVector parseLine(const string &inputline) {
+// разбить строку регулярным выражением на токены
+StringVector parseLine(const string &inputline, const char *pattern = "\\s+") {
+    std::regex rgx(pattern);
+    //std::regex rgx("(\\d+)");
+    std::sregex_token_iterator iter(inputline.begin(), inputline.end(),
+        rgx, -1);
+    std::sregex_token_iterator end;
     vector<string> retArray;
-    string delim = " ";  
-    int pos = 0;
-    size_t index= 0;
     
-    while (true) {
-        index = inputline.find(delim, pos);
-        cout << "index: " << index << ", pos: " << pos << endl; 
-        string token = inputline.substr(pos, index);
-        cout << "sub ='" << token << "'" << endl;
-        retArray.push_back(token);
-        pos = index + 1;
-        if (index == inputline.npos) {
-            cout << "end of line reached." << endl;
-            break;
-        }
-    }
-        
+    for ( ; iter != end; ++iter)
+        retArray.push_back(*iter);
+    
     return retArray;
 }
 
-void printIntInputLine(const StringVector& arr) {
+void printIntInputLine(const StringVector &arr) {
     cout << "printIntInputLine()" << endl;
+    cout << "size = " << arr.size() << endl;
     for (int i = 0; i < arr.size(); ++i) {
-        cout << "arr[" << i << "] = " << arr[i] << endl;
+        if (i + 1 < arr.size())
+            cout << arr[i] << ", ";
+        else
+            cout << arr[i];
     }
+    cout << endl;
     cout << "end of printIntInputLine()" << endl;
 }
 
+/*
 int main() {
     int n = 0, q = 0;
         
     string line;
     StringVector arr;
     
+    // line = "";
     getline(cin, line);        
     arr = parseLine(line);
     printIntInputLine(arr);
     
+    // line = "";
     getline(cin, line);        
     arr = parseLine(line);
     printIntInputLine(arr);
     
+    // line = "";
     getline(cin, line);        
     arr = parseLine(line);
     printIntInputLine(arr);
     
     cout << n << endl << q << endl;
     
-    /*
     std::istringstream input;
     input.str("1\n2\n3\n4\n5\n6\n7\n");
     int sum = 0;
@@ -80,8 +82,7 @@ int main() {
         std::cout << line << '\n';
     }
     
-    */
-    
     return 0;
 }
+*/
 
